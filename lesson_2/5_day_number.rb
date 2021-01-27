02c07a5687f16092222862009090a4da7582b81e
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-01-27 18:30:21 +0200

    5 - day number

---
+++ lesson_2/5_day_number.rb
@@ -0,0 +1,20 @@
+puts 'введите число (1-31): '
+day = gets.chomp.to_i
+
+puts 'введите месяц (1-12): '
+month = gets.chomp.to_i
+
+puts 'введите год: '
+year = gets.chomp.to_i
+
+days_mounths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+month_count = days_mounths.take(month - 1)
+day_count_month = month_count.sum
+
+leap_year = 29 if (year % 4).zero? && !(year % 100).zero? || (year % 400).zero?
+
+day_count = day + day_count_month
+day_count += 1 if leap_year && month > 2
+
+puts "\n#{day}.#{month}.#{year} является #{day_count} в #{year} году!"
