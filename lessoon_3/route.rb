a8aeae334bc5b607c627b0a4799e24d9e15c51bd
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-01-29 15:50:10 +0200

    add and delete intermediate station

--- lessoon_3/route.rb
+++ lessoon_3/route.rb
@@ -6,6 +6,22 @@ class Route
     @stations = [from_station, to_station]
   end
 
+  def add_station(station)
+    stations.insert(-2, station)
+  end
+
+  def first_station?(station)
+    station != stations.first
+  end
+
+  def last_station?(station)
+    station != stations.last
+  end
+
+  def dellet_station(station)
+    stations.delete(station) if first_station?(station) && last_station?(station)
+  end
+
   def list
     self.stations.each.with_index(1) do |station, index|
       puts "#{index}.  #{station.name}"
