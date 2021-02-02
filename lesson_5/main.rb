f09dbad46c3dfb32ba89eaf42b66d7c37ef82492
Elena Parchevskaya <elena.lemoon@gmail.com>
2021-02-02 18:32:59 +0200

    correct metod start

--- lesson_5/main.rb
+++ lesson_5/main.rb
@@ -13,25 +13,28 @@ class Interface
   trains = []
   routes = []
 
-  loop do
-    puts '==========================================='
-    puts '0 - Выход'
-    puts '1 - Действия со станциями'
-    puts '2 - Действия с поездами'
-    puts '3 - Действия с маршрутами'
-    puts '==========================================='
-    option = gets.chomp.to_i
-    case option
-    when 1
-      station_actions(stations)
-    when 2
-      trains_actions(trains, routes)
-    when 3
-      route_actions(routes, stations)
-    else
-      break if option.zero?
+  def start
+    loop do
+      puts '==========================================='
+      puts '0 - Выход'
+      puts '1 - Действия со станциями'
+      puts '2 - Действия с поездами'
+      puts '3 - Действия с маршрутами'
+      puts '==========================================='
+      option = gets.chomp.to_i
+      case option
+      when 1
+        station_actions(stations)
+      when 2
+        trains_actions(trains, routes)
+      when 3
+        route_actions(routes, stations)
+      else
+        break if option.zero?
+      end
     end
   end
+
 
   def station_actions(stations)
     loop do
